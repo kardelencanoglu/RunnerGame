8c576adf8ae8bce08e345f9a746942e9acb395d7
Kardelen Canoğlu <kardelen.canoglu7@gmail.com>
2023-02-13 22:27:25 +0300

    v1 runner

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+
+public class CollectCoin : MonoBehaviour
+{
+
+    public int score;
+    public TextMeshProUGUI CoinText;
+    public PlayerController playerController;
+    public int maxScore;
+    public Animator PlayerAnim;
+    public GameObject Player;
+    public GameObject EndPanel;
+
+    private void Start()
+    {
+        PlayerAnim = Player.GetComponentInChildren<Animator>();
+    }
+
+    public void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("Finish!...");
+            playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+            EndPanel.SetActive(true);
+
+            /*           if (score >= 5)
+                       {
+                           Debug.Log("You Win!");
+                       }*/
+            if (score >= maxScore)
+            {
+                //           Debug.Log("You Win!");
+                PlayerAnim.SetBool("win", true);
+            }
+            else
+            {
+                //               Debug.Log("You Lose!");
+                PlayerAnim.SetBool("lose", true);
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Touched Obstacle!...");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void AddCoin()
+    {
+        score++;
+        CoinText.text = "Score: " + score.ToString();
+
+    }
+}
